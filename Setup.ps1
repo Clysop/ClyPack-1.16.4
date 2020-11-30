30cd3022c96e021526f256571088d25168040cb9
Espen Andreas Larsen <clysop@gmail.com>
2020-11-30 18:55:48 +0100

    Updated setup

--- Setup.ps1
+++ Setup.ps1
@@ -10,22 +10,27 @@ Invoke-WebRequest -Uri $url -OutFile ($output + ".zip")
 Write-Output "Download complete."
 Write-Output "Extracting modpack..."
 
-Rename-Item "mods" "mods_old"
+if (Test-Path "mods") {
+  Rename-Item "mods" "mods_old"
+} else {
+  New-Item -Name "mods_old" -ItemType "directory"
+}
+
 Expand-Archive ($output + ".zip") -Force
-Copy-Item ($output + "/*/*") "." -Force -Recurse
+Copy-Item ($output + "\*\*") "." -Force -Recurse
 Remove-Item $output -Recurse
-Remove-Item ($output + ".zip") -Recurse
+Remove-Item ($output + ".zip")
 
 Write-Output "Extraction complete."
 Write-Output "Downloading mods...`n"
 
-Move-Item "mods/*" "mods_temp" -Force
+Rename-Item "mods" "mods_temp"
 Rename-Item "mods_old" "mods"
 
 java -jar InstanceSync.jar
 
-Move-Item "mods_temp/*" "mods"
-Remove-Item "mods_*"
+Move-Item "mods_temp\*" "mods" -Force
+Remove-Item "mods_*" -Recurse
 
 Write-Output "`nSetup complete."
 Read-Host "Press ENTER to continue..."
